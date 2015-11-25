0dbb47beab4c5ed949c5ac483aaf305f319a83d1
Michael Neild <michaeljneild@gmail.com>
2015-11-25 23:12:16 +1300

    Enabled editing and deleting assignments

---
+++ ContosoUniversity/ContosoUniversity/Models/Assignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Assignment
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public string CompletionInfo { get; set; }
+        public double Percentage { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}
